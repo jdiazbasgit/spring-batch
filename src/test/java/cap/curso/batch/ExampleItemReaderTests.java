67cd1f82457cc38f1da26b3de354ba79c603aea4
Tu Nombre <you@example.com>
2021-07-20 09:07:41 +0200

    carga inicial

---
+++ src/test/java/cap/curso/batch/ExampleItemReaderTests.java
@@ -0,0 +1,20 @@
+package cap.curso.batch; 
+
+import cap.curso.batch.ExampleItemReader;
+
+import junit.framework.TestCase;
+
+public class ExampleItemReaderTests extends TestCase {
+
+	private ExampleItemReader reader = new ExampleItemReader();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", reader.read());
+	}
+
+	public void testReadTwice() throws Exception {
+		reader.read();
+		assertEquals(null, reader.read());
+	}
+
+}
